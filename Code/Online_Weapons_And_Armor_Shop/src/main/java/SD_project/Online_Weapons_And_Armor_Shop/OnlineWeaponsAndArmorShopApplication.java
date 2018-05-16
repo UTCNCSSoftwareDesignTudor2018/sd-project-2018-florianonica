0d6ca8e64040ebb5dc56d8aed47e38a929b75a21
Nicolae-Florian Onica <onicaflorin96@gmail.com>
2018-05-16 19:07:14 +0300

    Initial

---
+++ Code/Online_Weapons_And_Armor_Shop/src/main/java/SD_project/Online_Weapons_And_Armor_Shop/OnlineWeaponsAndArmorShopApplication.java
@@ -0,0 +1,12 @@
+package SD_project.Online_Weapons_And_Armor_Shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineWeaponsAndArmorShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineWeaponsAndArmorShopApplication.class, args);
+	}
+}
---
+++ Code/Online_Weapons_And_Armor_Shop/src/test/java/SD_project/Online_Weapons_And_Armor_Shop/OnlineWeaponsAndArmorShopApplicationTests.java
@@ -0,0 +1,16 @@
+package SD_project.Online_Weapons_And_Armor_Shop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlineWeaponsAndArmorShopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
